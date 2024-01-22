ce6186140a435c954366c355ae6357b50d94bbe5
kbtz <contact@kbtz.dev>
2024-01-22 04:48:54 -0300

    remove test page from source

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.css?inline'
-
-type bit = 0 | 1
-type flags = Record<string, boolean>
--- index.ts
+++
@@ -1,5 +0,0 @@
-import InputScale from './input-scale';
-
-InputScale.defaults.theme = '/themes/star-sparkle.svg'
-
-customElements.define('input-scale', InputScale)
